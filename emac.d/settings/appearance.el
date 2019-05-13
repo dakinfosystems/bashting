711e19dbea0d5352ca57b5268fdaa6937573c16a
Pramod Jain <dakinfosystems@users.noreply.github.com>
2019-05-13 17:51:16 +0530

    Create appearance.el

---
+++ emac.d/settings/appearance.el
@@ -0,0 +1,15 @@
+;;=========================================================================
+;; General Settings
+;;=========================================================================
+
+;; Loading theme
+(load-theme 'adwaita)
+
+;; setting line number
+(setq line-number-mode t)
+
+;; Set column number mode
+(setq column-number-mode t)
+
+;; It is to to provide feature is require is used
+(provide 'appearance)
